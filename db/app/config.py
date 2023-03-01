7cfd3271c4734a45149a194cdfc80e2b9c28a4ea
ahmedtaaw <ahmedtaaw@gmail.com>
2023-03-01 15:28:39 +0400

    fgfx/connection issues

--- db/app/config.py
+++ db/app/config.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from urllib.parse import quote 
 
-DATABASE_URL="postgres://postgres:postgres@localhost:5432/tdb"
+DATABASE_URL="postgresql://tuser:postgresql@localhost:5432/tdb"
+DATABASE_PWD="11223344"
 
-engine=create_engine(DATABASE_URL)
+
+engine=create_engine("postgresql://tuser:%s@localhost:5432/tdb" % quote("11223344"))
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- db/app/main.py
+++ db/app/main.py
@@ -1,14 +1,9 @@
 from fastapi import FastAPI
-import model
+import models
+from routes import router
 from config import engine
-import router
 
-model.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-@app.get("/")
-async def Home():
-    return {"Hello": "World"}
-
-app.include_router(router.router,prefix="/book",tags=["book"])
+app.include_router(router,prefix="/tuser",tags=["tuser"])
