01907e789993efdd8fc1db5cc41c3fc5da9e07fb
ahmedtaaw <ahmedtaaw@gmail.com>
2023-03-01 14:45:00 +0400

    ftr/create router

--- db/app/main.py
+++ db/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import model
 from config import engine
+import router
 
 model.Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,5 @@ app = FastAPI()
 @app.get("/")
 async def Home():
     return {"Hello": "World"}
+
+app.include_router(router.router,prefix="/book",tags=["book"])
