24cde5709fe1321b7aff6b0d64de7fb83d196f3b
ahmedtaaw <ahmedtaaw@gmail.com>
2023-03-02 09:31:58 +0400

    fgfx/config vars

--- db/app/config.py
+++ db/app/config.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from urllib.parse import quote 
 
-DATABASE_URL="postgresql://tuser:postgresql@localhost:5432/tdb"
+DATABASE_URL="postgresql://tuser:%s@localhost:5432/tdb"
 DATABASE_PWD="11223344"
 
 
-engine=create_engine("postgresql://tuser:%s@localhost:5432/tdb" % quote("11223344"))
+engine=create_engine( DATABASE_URL % quote(DATABASE_PWD))
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
