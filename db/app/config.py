da357d7fb87d3f5c2f4d06736fa29ff2995dc179
ahmedtaaw <ahmedtaaw@gmail.com>
2023-03-02 13:55:43 +0400

    update

--- db/app/config.py
+++ db/app/config.py
@@ -4,11 +4,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from urllib.parse import quote 
 
 #DATABASE_URL="postgresql://tuser:%s@localhost:5432/tdb"
-DATABASE_URL="postgresql://tuser:%s@172.18.0.3:5431/tdb"
-DATABASE_PWD="11223344"
+#DATABASE_URL="postgresql://tuser:%s@172.18.0.3:5050/tdb"
+#DATABASE_PWD="11223344"
+db_name = 'tdb'
+db_user = 'tuser'
+db_pass = '11223344'
+db_host = '172.18.0.2'
+db_port = '5431'
 
-
-engine=create_engine( DATABASE_URL % quote(DATABASE_PWD))
+db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+#engine=create_engine( DATABASE_URL % quote(DATABASE_PWD))
+engine=create_engine(db_string)
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
